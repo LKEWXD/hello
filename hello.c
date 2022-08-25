e68fc2bbc0caa5d515d7ac87ba04d30174b016fa
LKEWXD <lwooten5853@gmail.com>
2022-08-25 16:32:18 +0000

    idk what im doing but I hope this works

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
  * Sophia Birch
  * sbirch@unomaha.edu
- * 
+ *
  * 2022/08/23
  */
  #include <stdio.h>
  #include <stdlib.h>
- 
+
  int main(int argc, char **argv){
-     
+
      printf("Hello World!!!!\n");
      printf("Sophia Birch, Computer Engineering and Computer Science\n");
-     
-     
+     printf("Luke Wooten, Computer Engineering");
+
  }
